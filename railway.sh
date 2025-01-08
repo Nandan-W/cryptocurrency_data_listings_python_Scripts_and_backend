71c31cae6ffd25d1b89bbc1cff7c39501a9b1885
Fabreeeze <nandanwahi@gmail.com>
2025-01-09 01:37:09 +0530

    update railway.sh

--- railway.sh
+++ railway.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the web server and log output
-gunicorn app:app --bind 0.0.0.0:$PORT --daemon --access-logfile access.log --error-logfile error.log
+gunicorn app:app --bind 0.0.0.0:$PORT --daemon 
 
 # Start the worker process and log output
-python python_script_for_dataFetch_analysis_and_update.py > worker.log 2>&1
+python python_script_for_dataFetch_analysis_and_update.py 
